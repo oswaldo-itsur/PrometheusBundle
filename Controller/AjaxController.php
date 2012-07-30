d94abe9021ce6c3c5cfeb6f18e7d6377dfb239dc
Hugo <kakistos741@gmail.com>
2012-07-30 13:03:20 -0500

    Actualizacion

---
+++ Controller/AjaxController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Informatica\PrometheusBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/ajax")
+ */
+class AjaxController extends Controller
+{
+     
+    /**
+     * @Route("/profesores")
+     * @Template()
+     */
+    public function profesoresAction()
+    {
+       $request = $this->getRequest();
+       $AJAXResponse = array('Blanca','Armando','Gamez');
+       
+       if ($request->isXmlHttpRequest() == true) {
+       
+            $response = new Response(json_encode($AJAXResponse));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        }
+       
+       return new Response($AJAXResponse[0]);
+    }
+    
+    /**
+     * @Route("/alumnos")
+     * @Template()
+     */
+    public function alumnosAction()
+    {
+      
+    }
+    
+   
+}
